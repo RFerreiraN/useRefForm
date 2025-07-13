9812b5a3ca01aea0de7551864b82fe0a26d48b66
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-13 23:23:03 +0200

    logica

---
+++ src/Hooks/useFormRef.js
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+
+export const useFormRef = () => {
+
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = inputValue;
+
+  const handleInput = ({ target }) => {
+    const { name, value } = target;
+    setInputValue({
+      ...inputValue,
+      [name]: value
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (
+      !inputValue.email?.trim() ||
+      !inputValue.password?.trim()
+    ) return
+    console.log(inputValue)
+    resetForm()
+  }
+
+  const resetForm = () => {
+    setInputValue({
+      email: '',
+      password: ''
+    })
+  }
+
+  return {
+    handleInput,
+    onSubmit,
+    resetForm,
+    inputValue
+  }
+}
