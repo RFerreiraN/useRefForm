015c1b92c8aa5c6227c090647670f63f90931dd7
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-13 23:22:48 +0200

    creamos un custom hook para la logica

--- src/Components/UseRefForm.jsx
+++ src/Components/UseRefForm.jsx
@@ -1,43 +1,26 @@
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
+import { useFormRef } from '../Hooks/useFormRef';
 
 export const UseRefForm = () => {
-  const [inputValue, setInputValue] = useState({
-    email : '',
-    password : ''
+
+  const {onSubmit, resetForm, handleInput, inputValue} = useFormRef({
+  
   })
 
   const { email, password } = inputValue;
 
-  const handleInput = ({target}) => {
-    const { name, value } = target;
-    setInputValue({
-      ...inputValue,
-      [name] : value
-    })
-  }
+  const focusRef = useRef()
 
-  const onSubmit = (event) => {
-    event.preventDefault()
-    if(
-      !inputValue.email?.trim() ||
-      !inputValue.password?.trim()
-    ) return
-    console.log(inputValue)
-    resetForm()
-  }
-
-  const resetForm = () => {
-    setInputValue({
-      email : '',
-      password : ''
-    })
-  }
+  useEffect(() => {
+    focusRef.current.focus()
+  } , [])
 
   return (
     <form onSubmit={onSubmit}>
       <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
         <input
+          ref={focusRef}
           type="email"
           className="form-control"
           name="email"
